f1678ab087c8ffa41a4b47d92ca9915cea7a29f0
unknown <ajay.rai.1993@gmail.com>
2021-10-28 05:23:08 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-import { My } from './My';
 
-function App() {
-  return (
-    <>
-      <My/>
-    </>
-  );
+import React ,{ Component } from 'react';
+import './style.css';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      data:null
+    }
+
+  }
+  componentDidMount(){
+    fetch('https://api.opensea.io/api/v1/assets?format=json&limit=20&offset=0&order_direction=desc').then((res)=>{
+      res.json().then((result)=>{
+        this.setState({data:result.assets})
+      })
+    })
+  }
+  render(){
+   
+      return (
+        <>
+          {
+            this.state.data?
+            this.state.data.map((item)=>
+               <><div className="card sm y-3" key={item.id}>
+                <div>
+                  <img className="card-img-top" src={item.image_url} alt="" />
+                  <div className="card-body">
+                    <label htmlFor="">Name</label><h5 className="card-title" style={{ color: "red", }}>{item.name}</h5>
+                    <p className="card-text" style={{ color: "blue", }}>{item.description}</p>
+                  </div>
+                </div>
+              </div><br /></>
+            )
+            :
+            <div>isloading .......</div>
+          }
+        </>
+      );
+    }
 }
 
 export default App;
